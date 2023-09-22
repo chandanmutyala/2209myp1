9a1d907a12c4a31f4fb8e1643dea49778f3aac4c
chandanmutyala <chandanmutyala@gmail.com>
2023-09-22 16:19:01 +0530

    variables

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+    public static void main(String [] args){
+        System.out.println("Hello world ");
+        int age = (int) 2147483649 ;
+
+        System.out.println(age);
+
+
+    }
+}
